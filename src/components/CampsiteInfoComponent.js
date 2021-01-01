f3278586d1d9d9eded761f9eafd977107e82835d
Julian Cannon <cannonj55@gmail.com>
2021-01-01 18:17:34 -0500

    Presentational and Container Compopnents

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+
+class CampsiteInfo extends Component {
+    renderCampsite(campsite){
+        return(
+            <div class="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+    }
+
+    renderComments(comments){
+        if(comments){
+            return (
+                <div class="col-md-5">
+                    <h4>Comments</h4>
+                    {comments.map(comment => <div key={comment.id}> {comment.text} </div>)}
+                    {comments.map(comment => <div key={comment.id}> {comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} </div>)}
+                </div>
+            )
+        }
+    }
+
+    render() {
+            if(this.props.campsite) {
+                return (
+                    <div class="container">
+                    <div class="row">
+                    {this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                    </div>
+                    
+                   
+                );
+            } return <div />;
+            
+    }
+
+}
+
+export default CampsiteInfo;
